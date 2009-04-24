3ce2463b5e37521d0278f91bc1fe2242c4a065d0
Tony Wolf <wolf@os-forge.net>
2009-04-24 18:02:29 +0200

    * improve smarty integration    >> add SmartyFunctions class where we will provide some smarty-extensions    >> fix title-typo    >> introduce index.tpl as central "display"-file  * rename http folder to public  * introduce lib-folder    >> a files in this forder are classes!    >> move core/template_api.php to lib/Template.lib.php  * changed include path to use core and lib, every path in the $g_fs_paths var

---
+++ lib/SmartyFunctions.lib.php
@@ -0,0 +1,38 @@
+<?php
+# MantisBT - a php based bugtracking system
+
+# MantisBT is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# MantisBT is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with MantisBT.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package CoreAPI
+ * @subpackage TemplateAPI
+ * @copyright Copyright (C) 2009  Tony Wolf <wolf@os-forge.net>
+ * @link http://www.mantisbt.org
+ */
+
+/**
+ * Description of SmartyFunctionslib
+ *
+ * @author dragon-fire
+ */
+class SmartyFunctions {
+  
+	public static function compiler_lang( &$p_tag_args, &$p_smarty) {
+		$t_tag = trim( $p_tag_args );
+
+		echo ' echo \''.lang_get($t_tag).'\';';
+	}
+
+}
+?>
